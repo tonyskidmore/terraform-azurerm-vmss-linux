88b476f090ffdee805c44ce3767fad457a5c2731
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2021-11-18 15:37:49 +0000

    adding something

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_linux_virtual_machine_scale_set" "example" {
-  name                = var.name
+   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = var.sku
